57bfb246a2fd65b2491747b93b9f784593305a5a
sohyun <sohyun32253@kaywon.ac.kr>
2024-08-02 14:58:46 +0900

    completed

---
+++ src/components/About.vue
@@ -0,0 +1,280 @@
+<template>
+<div id="about">
+            <v-container class="about_goal md fluid" >
+                <!-- brand goal -->
+                <div class="about_goal_cont">
+                    <div class="about_goal_left">
+                        <img src="@/assets/about_01.jpg" class="about_01_img">
+                    </div>
+
+                <div class="about_goal_right">
+                    <h2>BRAND GOAL</h2>
+                    <p class="dc_txt about_box_01">&#39; KEEP IT CLOSE &#39;</p>
+                    <p class="txt_01">국내산 프리미엄 소금 &#39;그레이 솔트&#39; 토판염 기반의 라이프스타일 브랜드</p>
+                    <p class="saltrain_logo dc_txt">Saltrain</p>
+                    <p class="txt_02 line_h">  
+                        먹을 수 있는 소금을 바탕으로 한 라이프스타일 프로덕트를 보다 &#39;더 가까이에서&#39; <br>
+                        즐길 수 있도록 돕는 것은 변함없는 솔트레인의 목표입니다&#46;
+                    </p>
+                    <img src="@/assets/about_02.jpg" class="about_02_img">
+                 </div>
+                </div>
+            </v-container>
+
+            
+
+            <!-- brand value -->
+            <v-container class="about_value fluid">
+                <div class="about_value_cover"></div>
+                    <div class="about_value_cont">
+                        <h2>BRAND VALUE</h2>
+                        <p class="dc_txt about_box_02">&#39; Life essentials make you GOOD &#39;</p>
+                        <p class="line_h mb_32">솔트레인이 정의하는 &#39;좋음&#40;GOOD&#41;&#39;은 사용자가 우리의 제품을<br>
+                            보고&#44; 쓰고&#44; 입고&#44; 바르는 모든 과정에서 얻는 긍정적인 감정을 의미합니다&#46;
+                        </p>
+                        <p class="line_h mb_32">
+                            매일 사용할 수 밖에 없는 생활 필수품에 대한 진정한 만족감을 위해 솔트레인은 <br>
+                            원료 선택부터 디자인까지&#44; 모든 과정에서 최고의 퀄리티를 우선시 합니다&#46;
+                        </p>
+                        <p class="line_h">
+                            꾸미기보다 가꾸기에 집중하는 일상의 작은 돌봄&#46; <br>
+                            기분 좋은 에센셜 아이템을 통해 더욱 만족스러운 라이프를 누리는 것&#46; <br>
+                            감각적인 당신을 위해 솔트레인이 제공하는 궁극적인 가치입니다&#46;
+                        </p>
+                </div>
+            </v-container>   
+         
+
+            <v-container class="about_material fluid">
+                <!-- material -->
+                <div class="material_left">
+                    <img src="@/assets/about_04.jpg" class="about_material_img">
+                </div>
+                
+                <div class="material_right">
+                    <h2>MATERIAL</h2>
+                    <p class="dc_txt about_box_03">&#39; GRAY SALT &#39;</p>
+                    <p class="line_h mb_32">
+                        갯벌을 다져 만든 흙판에서 생산하는 천일염으로 <br>
+                        장인이 자연 갯벌을 다져 전통 방식으로 소량 생산한 소금입니다&#46;
+                    </p>
+                    <p class="line_h mb_32">
+                        국제 미식 품평회&#40;ITQI&#41; &#34;2 STAR&#34;&#44; <br>
+                        세계적 인증심사&#40;Superior Taste Award&#41;를 수상 받았습니다&#46;
+                    </p>
+                    <p class="line_h">
+                        미네랄 함량 UP 나트륨 DOWN<br>
+                        다양한 유기화합물의 영양분과 미네랄을 다량 함유하고 있습니다&#46;
+                    </p>
+                </div>
+            </v-container>
+            <p class="dc_txt S_logo">Saltrain</p>
+
+    </div>
+</template>
+
+<style scoped>
+
+/* pc 1280px */
+
+
+.dc_txt{font-size:  var(--h1-font-size); 
+    font-family: "urbane-condensed", sans-serif; 
+    font-weight: 700; font-style: normal;
+   }
+
+#about{
+    width: 100%;
+    background: #161616;
+    height: 2730px;
+    position: relative;
+}
+
+
+v-main{
+   width: 100%;
+   background: #161616;
+   color: #fff;
+}
+
+h2{
+    margin-bottom: 32px;
+}
+
+p{
+    font-size: 1.25rem;
+}
+
+.line_h{
+    line-height: 1.5;
+}
+
+/* about_goal */
+v-container{
+    display: block;
+    width: 1280px;
+    margin: 0 auto ;
+}
+
+
+/*
+.about_goal{
+    max-width: 1280px;
+    margin: 5rem auto 0;
+    margin-bottom: 7.5rem;
+}*/
+
+.about_goal_box{
+    margin-bottom: 3.75rem;
+}
+
+
+.about_goal_cont{
+    display: flex;
+    padding: 5rem 0 7.5rem;
+}
+
+.about_goal_right{
+    margin-left: 100px;
+}
+
+.about_01_img{
+    width: 505px;
+    height: 632px;
+}
+
+.about_02_img{
+    width: 635px;
+    height: 410px;
+}
+
+.about_box_01{
+    width: 329px;
+    background:#1A51E6;
+    padding: 12px 10px 3px 20px;
+    margin-bottom: 3.75rem;
+}
+
+.saltrain_logo{
+    margin-top: 2.5rem;
+    margin-bottom: 1rem;
+    font-size: 4rem;
+}
+
+.txt_02{
+    margin-bottom: 132px;
+}
+
+/* about_value */
+.about_value{
+    width: 100%;
+    height: 692px;
+    background: url(../assets/about_bg.jpg) no-repeat;
+    background-size: cover;
+    position: relative;
+    z-index: 0;
+}
+
+
+.about_value_cover{
+    width: 100%;
+    height: 692px;
+    background: rgba(0, 0, 0, 0.6);;
+    position: absolute;
+    top: 0;
+    z-index: 1;
+}
+
+.about_value_cont{
+    padding: 7.5rem 0;
+    width: 1280px;
+    margin: 0 auto;
+    text-align: center;
+    position: relative;
+    z-index: 2;
+}
+
+.about_box_02{
+    width: 640px;
+    margin: 0 auto 3.75rem;
+    background:#1A51E6;
+    padding: 15px 12px 10px 15px;
+}
+
+.mb_32{
+    margin-bottom: 32px;
+}
+
+/* material */
+
+.about_material{
+    width: 1280px;
+    margin: 0 auto;
+    display: flex;
+    padding: 7.5rem 0;
+}
+
+.about_material_img{
+    width: 505px;
+    height: 470px;
+    margin-right: 100px;
+}
+
+.about_box_03{
+    width: 257px;
+    margin-bottom: 3.75rem;
+    background:#1A51E6;
+    padding: 15px 12px 8px 15px;
+}
+
+#about .S_logo{
+    font-size: 140px;
+    position: absolute;
+    right: 134px;
+    bottom: 120px;
+}
+
+/* mobile 600px */
+
+@media (max-width: 600px){
+    p{
+        font-size: 18px;
+    }
+    .S_logo{
+        display: none 
+    }
+   #about .about_goal_cont{
+        display: block;
+        text-align: center;
+    }
+    .about_goal_right{
+        margin: 0;
+       max-width: 600px;
+    }
+    .about_box_01{
+        margin: 0 auto;
+    }
+    .about_01_img{
+        width: auto;
+        height: auto;
+        margin-bottom: 80px;
+    }
+    .about_value_cont{
+        max-width: 600px;
+        font-size: 32px;
+    }
+    #about .about__material{
+        display: block;
+        text-align: center;
+    }
+}
+
+
+
+</style>
+
+<script>
+    export default{
+
+    }
+</script>
