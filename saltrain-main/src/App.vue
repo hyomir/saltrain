63a60c19ffc323385e2cfda5ab2fb6443915581b
down <wjdekdns015@naver.com>
2024-08-08 17:17:03 +0900

    test

---
+++ saltrain-main/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      hello
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+<style>
+@import url('../src/assets/reset.css');
+@import url('../src/assets/common.css');
+</style>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
